14747540376b075ec599d9b03719f69040d4a181
TheRajeshB <90644503+TheRajeshB@users.noreply.github.com>
2021-09-16 11:16:13 -0400

    Update Lab01_Q2_abc.py

--- Lab_1/Lab01_Q2_abc.py
+++ Lab_1/Lab01_Q2_abc.py
@@ -146,3 +146,92 @@ Q2(duration = 20.0, # duration of simulation in Earth years
     earth_v_x = 0.0, #initial x-velocity in AU/yr
     earth_v_y = 3.46, #initial y-veloctiy in AU/yr
     )
+
+
+#FARZ'S VERSION 
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+def three_body_problem():
+    
+    #setting up time step for integration 
+    delta_t = 0.0001
+    years   = 3.0 #number of years over which values are calculated
+    n_steps = int(years*1/delta_t) #number of steps for numerical integration
+    
+    #creating lists to store output at each step of EC integration 
+    E_position_x = [1.0] #list to store x-position of Earth
+    E_position_y = [0.0] #list to store y-position of Earth
+    J_position_x = [5.2] #list to store x-position of Jupiter
+    J_position_y = [0.0] #list to store y-position of Jupiter 
+    
+    
+    
+    #setting constants 
+    M_sun = 1 # mass of sun in solar masses
+    G = 39.5 # gravitational constant in solar masses, AU and yr as units
+    M_Jupiter = 1 #mass of Jupiter in solar masses
+    
+    #setting initial conditions for Earth 
+    x_E_initial     =  1.0 #initial Earth x-position in AU 
+    y_E_initial     =  0.0 #initial Earth y-psoition in AU 
+    v_E_initial_x   =  0.0 #initial x-component of Earth velocity in AU/yr
+    v_E_initial_y   =  6.18 #initial y-component of Earth velocity in AU/yr
+    
+    #setting initial conditions for Jupiter
+    x_J_initial   = 5.2  #initial Jupiter x-position in AU 
+    y_J_initial   = 0.0  #initial Jupiter y-position in AU
+    v_J_initial_x = 0.0  #initial x-component of Jupiter velocity in AU/yr
+    v_J_initial_y = 2.63 #initial y-component of Jupiter velocity in AU/yr
+    
+    #loop to carry out numerical integration
+    for i in range(0,n_steps):
+        
+        #defining distance of Jupiter from Sun(r_J)
+        r_J = ((x_J_initial)**2 + (y_J_initial)**2)**0.5
+        
+        #defining distance of Earth from Sun (r_E)
+        r_E = ((x_E_initial)**2 + (y_E_initial)**2)**0.5 
+        
+        #defining distance between Earth and Jupiter
+        r_E_J = ((x_E_initial-x_J_initial)**2 + (y_E_initial-y_J_initial)**2)**0.5
+        
+        #determining new velocity components for Earth using EC method
+        v_E_next_x = v_E_initial_x - (G*M_sun*x_E_initial/r_E**3)*delta_t + (G*M_Jupiter*(x_J_initial-x_E_initial)/r_E_J**3)*delta_t
+        v_E_next_y = v_E_initial_y - (G*M_sun*y_E_initial/r_E**3)*delta_t + (G*M_Jupiter*(x_J_initial-x_E_initial)/r_E_J**3)*delta_t
+        
+        #determining new velocity components for Jupiter, ignoring influence of Earth 
+        v_J_next_x = v_J_initial_x - (G*M_sun*x_J_initial)/r_J**3*delta_t
+        v_J_next_y = v_J_initial_y - (G*M_sun*y_J_initial)/r_J**3*delta_t
+        
+        #determining new components for Earth position
+        x_E_next = x_E_initial + v_E_next_x*delta_t
+        y_E_next = y_E_initial + v_E_next_y*delta_t
+        
+        #determining new components for Jupiter position
+        x_J_next = x_J_initial + v_J_next_x*delta_t
+        y_J_next = y_J_initial + v_J_next_y*delta_t
+        
+        #storing outputs in previously created lists
+        E_position_x.append(x_E_next)
+        E_position_y.append(y_E_next)
+        J_position_x.append(x_J_next)
+        J_position_y.append(y_J_next)
+        
+        #re-setting values for next step of integration
+        x_E_initial     = x_E_next
+        y_E_initial      = y_E_next
+        v_E_initial_x   = v_E_next_x
+        v_E_initial_y   = v_E_next_y
+        
+        x_J_initial     = x_J_next
+        y_J_initial     = y_J_next
+        v_J_initial_x   = v_J_next_x
+        v_J_initial_y   = v_J_next_y
+    
+    plt.errorbar(E_position_x,E_position_y)
+    plt.errorbar(J_position_x,J_position_y)
+    plt.show()
+
+print(three_body_problem())
