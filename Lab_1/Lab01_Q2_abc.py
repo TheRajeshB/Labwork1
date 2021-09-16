21db40424dfa91d84e9635208b0a69911068aec9
Gabriel Bailey <gabriel.ken.bailey@gmail.com>
2021-09-15 21:07:03 -0400

    Completed my code for 2b and 2c.

--- Lab_1/Lab01_Q2_abc.py
+++ Lab_1/Lab01_Q2_abc.py
@@ -11,20 +11,28 @@ This code does:
 import matplotlib.pyplot as plt
 import numpy as np
 
-def Q2a():
+def Q2( duration,
+        delta_t = 0.0001, #timestep in seconds
+        earth_x = 1.0, #initial x-position of Earth in AU 
+        earth_y = 0.0, #initial y - position of Earth in AU
+        earth_v_x = 0.0, #initial x-velocity in AU/yr
+        earth_v_y = 6.18, #initial y-veloctiy in AU/yr
+        jup_scale_factor = 1 #how much larger the mass of jupiter should be
+        ):
 
     # setting constants
     M_sun = 1 # mass of sun in solar masses
-    M_jup = 0.001 # mass of Jupiter in solar masses
+    M_jup = 0.001 * jup_scale_factor # mass of Jupiter in solar masses
     G = 39.5 # gravitational constant in solar masses, AU and yr as units
 
     #setting time-step and number of steps for integration
-    duration = 10.0 # duration of simulation in Earth years
-    delta_t = 0.0001
+    #duration = 10.0 # duration of simulation in Earth years
+    #delta_t = 0.0001
     n_steps = int(duration/delta_t)
 
     #creating lists to store data
     times = np.empty(n_steps)
+    earth_jup_r_list = np.empty(n_steps)
     jup_x_list = np.empty(n_steps) #list for x-values
     jup_y_list = np.empty(n_steps) #list for y-values
     jup_v_x_list = np.empty(n_steps) #list for x-velocities
@@ -45,10 +53,10 @@ def Q2a():
     jup_av = np.linalg.norm(np.cross((jup_x,jup_y),(jup_v_x,jup_v_y)))
 
     #setting inital conditions for Earth
-    earth_x = 1.0 #initial x-position of Earth in AU 
-    earth_y = 0.0 #initial y - position of Earth in AU
-    earth_v_x = 0.0 #initial x-velocity in AU/yr
-    earth_v_y = 6.18 #initial y-veloctiy in AU/yr
+    #earth_x = 1.0 #initial x-position of Earth in AU 
+    #earth_y = 0.0 #initial y - position of Earth in AU
+    #earth_v_x = 0.0 #initial x-velocity in AU/yr
+    #earth_v_y = 6.18 #initial y-veloctiy in AU/yr
     earth_av = np.linalg.norm(np.cross((earth_x,earth_y),(earth_v_x,earth_v_y))) #initial angular momentum in AU^2/yr
 
     #loop to carry out numerical integration
@@ -63,9 +71,9 @@ def Q2a():
         jup_av_list[i] = (jup_av) #list of angular-velocities at each step
 
         #defining 'r' 
-        jup_r = ((jup_x)**2 + (jup_y)**2)**0.5
-        earth_r = ((earth_x)**2 + (earth_y)**2)**0.5
-        earth_jup_r = ((earth_x-jup_x)**2 + (earth_y-jup_y)**2)**0.5
+        jup_r = ((jup_x)**2 + (jup_y)**2)**0.5 #distance from the Sun to Jupiter
+        earth_r = ((earth_x)**2 + (earth_y)**2)**0.5 #distance from the Sun to Earth
+        earth_jup_r = ((earth_x-jup_x)**2 + (earth_y-jup_y)**2)**0.5 #distance from Jupiter to Earth
         
         #new x velocity and position, uding Euler-Cromer integration 
         jup_v_x = jup_v_x - ((G*M_sun*jup_x)/(jup_r**3))*delta_t
@@ -79,6 +87,7 @@ def Q2a():
         jup_av = np.linalg.norm(np.cross((jup_x,jup_y),(jup_v_x,jup_v_y)))
 
         #Calculations for Earth
+        earth_jup_r_list[i] = earth_jup_r
         earth_x_list[i] = (earth_x) #list of x-values at each step
         earth_y_list[i] = (earth_y) #list of y-values at each step
         earth_v_x_list[i] = (earth_v_x) #list of x-velocities at each step
@@ -105,35 +114,34 @@ def Q2a():
 
     #x vs y position plot for Jupiter and Earth
     plt.plot(0,0, color = 'yellow', marker = 'o', label = "Sun", markersize = '5')
-    plt.errorbar(np.array(jup_y_list),np.array(jup_x_list), color = 'orange', label = "Jupiter")
-    plt.errorbar(np.array(earth_y_list),np.array(earth_x_list), color = 'b', label = "Earth")
+    plt.errorbar(jup_y_list,jup_x_list, color = 'orange', label = "Jupiter")
+    plt.errorbar(earth_y_list,earth_x_list, color = 'b', label = "Earth")
     plt.legend()
     plt.xlabel('y-position in AU')
     plt.ylabel('x-position in AU')
-    plt.title('plot of x vs y position of Jupiter and Earth')
+    plt.title('Plot of Position of Jupiter and Earth')
     plt.show()
 
     # Plot of distance from Earth to Jupiter over time
-    plt.errorbar(np.array(times),np.array(jup_x_list), color = 'b')
+    plt.errorbar(times,earth_jup_r_list, color = 'b')
     plt.xlabel('Time in Earth years')
     plt.ylabel('Distance in AU')
-    plt.title('Distance Between Jupiter and Earth')
+    plt.title('Distance Between Jupiter and Earth Over Time')
     plt.show()
-    
-    """#x-component of velocity vs time graph for Mercury
-    plt.errorbar(np.arange(0.0,1.0+delta_t,delta_t),earth_v_x_list)
-    plt.xlabel('Time in Earth year')
-    plt.ylabel("x-component of Mercury's velocity in AU/yr" )
-    plt.title('x-component of velocity vs time for Mercury')
-    plt.show()
-    
-    #y-component of velocity vs time graph for Mercury
-    plt.errorbar(np.arange(0.0,1.0+delta_t,delta_t),earth_v_y_list)
-    plt.xlabel('Time in Earth year')
-    plt.ylabel("y-component of Mercury's velocity in AU/yr" )
-    plt.title('y-component of velocity vs time for Mercury')
-    plt.show()"""
-    
-Q2a()
 
-#end of 2a
+#2a
+Q2(duration = 10.0, # duration of simulation in years
+    )
+
+#2b
+Q2(duration = 3.0, # duration of simulation in Earth years
+    jup_scale_factor = 1000
+    )
+
+#2c
+Q2(duration = 20.0, # duration of simulation in Earth years
+    earth_x = 3.3, #initial x-position of Earth in AU 
+    earth_y = 0.0, #initial y - position of Earth in AU
+    earth_v_x = 0.0, #initial x-velocity in AU/yr
+    earth_v_y = 3.46, #initial y-veloctiy in AU/yr
+    )
