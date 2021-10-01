b69713ce763cbc7528541fea513cf4ac48b00ffa
Gabriel Bailey <gabriel.ken.bailey@gmail.com>
2021-09-30 23:16:08 -0400

    Q2c now displays something

--- Lab03/lab03_Q1_c.py
+++ Lab03/lab03_Q1_c.py
@@ -35,14 +35,16 @@ def C(u,N,a,x,lamb,z):
        x = x-coordinate of position
        z = z coordinate of postion
        lamb = wavelength'''
-    C_values = []
-    for i in u(x,lamb,z):
-     b = i   #end points of integration
-     x1,w1 = gaussxwab(N,a,b)
-     s1 = 0.0
-     for k in range(N): #loop for Gaussian integration
-        s1 += w1[k]*f(x1[k])
-     C_values.append(s1)
+    C_values = np.empty([x.size,z.size])
+    for j in range(x.size):
+        for v in range(z.size):
+            i = u(x[j],lamb,z[v])
+            b = i   #end points of integration
+            x1,w1 = gaussxwab(N,a,b)
+            s1 = 0.0
+            for k in range(N): #loop for Gaussian integration
+                s1 += w1[k]*f(x1[k])
+            C_values[j,v] = s1
     return C_values
 
 def S(u,N,a,x,lamb,z):
@@ -55,25 +57,37 @@ def S(u,N,a,x,lamb,z):
        z = z coordinate of postion
        lamb = wavelength'''
        
-    S_values= []
-    for i in u(x,lamb,z):
-           b = i #end points of integration
-           x2,w2 = gaussxwab(N,a,b)
-           s2 = 0.0
-           for k in range(N): #loop for Gaussian integration
-               s2 += w2[k]*g(x2[k])
-           S_values.append(s2)
+    S_values = np.empty([x.size,z.size])
+    for j in range(x.size):
+        for v in range(z.size):
+            i = u(x[j],lamb,z[v])
+            b = i #end points of integration
+            x2,w2 = gaussxwab(N,a,b)
+            s2 = 0.0
+            for k in range(N): #loop for Gaussian integration
+                s2 += w2[k]*g(x2[k])
+            S_values[j,v] = s2
     return S_values 
 
-a = np.array([C(u,50,0.0,np.linspace(-3,10,10),1,np.linspace(1,5,10))])
-b = np.array([S(u,50,0.0,np.linspace(-3,10,10),1,np.linspace(1,5,10))])
-I1 = (((2*a + 1)**2) + ((2*b + 1)**2))/8
-list = []
-for i in I1 :
-    for j in i:
-        list.append(j)
-        
-print(list)
-x = np.linspace(-3,10,5)
-z = np.linspace(1,5,5)
-z,x = np.meshgrid(z,x)
+x = np.linspace(-3,10,20)
+z = np.linspace(1,5,20)
+a = C(u,50,0.0, x, 1, z)
+b = S(u,50,0.0, x, 1, z)
+aff=2*a 
+asgg=2*a + 1
+ads=((2*a + 1)**2)
+asdg=((2*b + 1)**2)
+I1 =np.add(((2*a + 1)**2), ((2*b + 1)**2))/8
+
+fig1, ax1 = plt.subplots(1,1)
+ax1.contourf(x,z,I1, 20)
+ax1.set_title('Intensity')
+ax1.set_xlabel('x')
+ax1.set_ylabel('z')
+
+fig2, ax2 = plt.subplots(1,1)
+ax2.pcolormesh(z, x, I1)
+ax2.set_title('Intensity')
+ax2.set_xlabel('x')
+ax2.set_ylabel('z')
+plt.show()
