c323b4ff127d8202613e806437762d2015f20156
Gabriel Bailey <gabriel.ken.bailey@gmail.com>
2021-10-28 15:59:30 -0400

    Q3 code more or less done

--- Lab07/Lab07_Q3.py
+++ Lab07/Lab07_Q3.py
@@ -1,116 +1,108 @@
-from math import inf, sin,pi
-import numpy as np#import empty,array,arange,isnan
+# Adapted from bulirsch.py
+"""Created on 2021-10-27
+
+@author: Gabriel Bailey
+
+This code adapts the code from bulirsch.py to work on an adaptive timestep, and simulates the Belousov--Zlzabotinsky  reaction, creating a plot of the chemical concentrations.
+"""
+import numpy as np
 import matplotlib.pyplot as plt
 
-t_init = 0.0
-t_final = 20.01
-H = 20 # Size of inital step
-delta = 1e-10     # Required position accuracy per unit time
-n_max = 8
+# Constants
 a = 1
 b = 3
-x = 0
-y = 0
+x0 = 0
+y0 = 0
+delta = 1e-10     # Required position accuracy per unit time
+n_max = 8
+
 # recursion step
 # else recall r t h, break it in half pg 3
 def f(con):
     x = con[0]
     y = con[1]
-    #print('f',x,y,1-(b+1)*x + a*x**2*y,(b)*x - a*x**2*y)
     vx = 1-(b+1)*x + a*x**2*y
     vy = b*x - a*x**2*y
     return np.array([vx,vy],float)
 
-def step(r, t, H, n):
-    r1 = r + H*f(r)
+def step(r, t, H):
+    # The actual arrays we are trying to fill with data
+    tpoints = []
+    rpoints = []
 
     # Do one modified midpoint step to get things started
     n = 1
     r1 = r + 0.5*H*f(r)
     r2 = r + H*f(r1)
-    #print('r',r1,r2)
 
-    # The array R1 stores the first row of the
-    # extrapolation table, which contains only the single
-    # modified midpoint estimate of the solution at the
-    # end of the interval
     R1 = np.empty([1,2],float)
     R1[0] = 0.5*(r1 + r2 + 0.5*H*f(r2))
 
-def full_calc(H):
-    tpoints = np.arange(t_init,t_final,H)
-    concentration = []
-    r = np.array([x,y],float)
+    # Now increase n until the required accuracy is reached
+    error = 2*H*delta
+    while error>H*delta:
+        n += 1
+        if n > n_max:
+            # Go to a smaller time step
+            t1,r1 = step(r, t, H/2)
+            t2,r2 = step(r1[-1], t+H/2, H/2)
+            # Place new data into existing lists
+            tpoints.extend(t1)
+            tpoints.extend(t2)
+            rpoints.extend(r1)
+            rpoints.extend(r2)
+            return tpoints,rpoints
+        h = H/n
 
-    # Do the "big steps" of size H
+        # Modified midpoint method
+        r1 = r + 0.5*h*f(r)
+        r2 = r + h*f(r1)
+        for i in range(n-1):
+            r1 += h*f(r2)
+            r2 += h*f(r1)
 
-    for t in tpoints:
+        # Calculate extrapolation estimates.  Arrays R1 and R2
+        # hold the two most recent lines of the table
+        R2 = R1
+        R1 = np.empty([n,2],float)
 
-        concentration.append(r)
-        #print(t,r)
+        R1[0] = 0.5*(r1 + r2 + 0.5*h*f(r2))
+        for m in range(1,n):
+            epsilon = (R1[m-1]-R2[m-1])/((n/(n-1))**(2*m)-1)
+            R1[m] = R1[m-1] + epsilon
+        if np.isnan(epsilon[0]) or np.isnan(epsilon[1]):
+            epsilon = np.array([np.inf,np.inf])
+        error = abs(epsilon[0])
 
-        # Do one modified midpoint step to get things started
-        n = 1
-        r1 = r + 0.5*H*f(r)
-        r2 = r + H*f(r1)
-        #print('r',r1,r2)
+    # Set r equal to the most accurate estimate we have,
+    # before moving on to the next big step
+    r = R1[n-1]
+    # Add current point to data lists
+    tpoints.append(t+H)
+    rpoints.append(r)
+    return tpoints,rpoints
 
-        # The array R1 stores the first row of the
-        # extrapolation table, which contains only the single
-        # modified midpoint estimate of the solution at the
-        # end of the interval
-        R1 = np.empty([1,2],float)
-        R1[0] = 0.5*(r1 + r2 + 0.5*H*f(r2))
+# Initial simulation conditions
+r0 = np.array([x0,y0],float)
+t0 = 0.0
+H = 20 # Size of inital step, also total size
 
-        # Now increase n until the required accuracy is reached
-        error = 2*H*delta
-        while error>H*delta:
-            
-            n += 1
-            if n > n_max:
-                return full_calc(H/2)
-            h = H/n
-            #print("error",error,"h",h)
+# Simulate
+tp,rp = step(r0, t0, H)
 
-            # Modified midpoint method
-            r1 = r + 0.5*h*f(r)
-            r2 = r + h*f(r1)
-            #print('in r',n,r1,r2)
-            for i in range(n-1):
-                r1 += h*f(r2)
-                r2 += h*f(r1)
-                #print('loop r',i,r1,r2)
+# Format for plotting
+tpoints = [t0] + tp
+concentration= [r0] + rp
+tpoints, concentration = np.array(tpoints), np.array(concentration)
 
-            # Calculate extrapolation estimates.  Arrays R1 and R2
-            # hold the two most recent lines of the table
-            R2 = R1
-            R1 = np.empty([n,2],float)
-
-            R1[0] = 0.5*(r1 + r2 + 0.5*h*f(r2))
-            # if isnan(R1[0][0]) or isnan(R1[0][1]):
-            #     R1[0] = array([inf,inf])
-            #print("EXCEPT",R1[0], r1, r2, h, f(r2))
-            for m in range(1,n):
-                epsilon = (R1[m-1]-R2[m-1])/((n/(n-1))**(2*m)-1)
-                R1[m] = R1[m-1] + epsilon
-            #print("eps",epsilon)
-            if np.isnan(epsilon[0]) or np.isnan(epsilon[1]):
-                epsilon = np.array([inf,inf])
-            error = abs(epsilon[0])
-
-        # Set r equal to the most accurate estimate we have,
-        # before moving on to the next big step
-        r = R1[n-1]
-    #print(concentration)
-    return tpoints, np.array(concentration)
-
-tpoints, concentration = full_calc(H)
-#print(tpoints,concentration)
+# size of h
+dtpoints = np.array(tpoints[1:]) - np.array(tpoints[:-1])
 
 # Plot the results
 plt.figure()
 plt.plot(tpoints,concentration[:,0],".-", label="x")
 plt.plot(tpoints,concentration[:,1],".-", label="y")
+plt.plot(tpoints[:-1], dtpoints, label = "Timestep Size (s)") # drop the last point in tpoints
 plt.xlabel("Time (s)")
 plt.ylabel("Chemical concentrations")
 plt.title('Belousov-Zhabotinsky Reaction over Time')
@@ -118,15 +110,5 @@ plt.legend()
 plt.grid()
 plt.tight_layout()
 plt.savefig('Q3.png', dpi=150)
-
-plt.figure()
-dtpoints = np.array(tpoints[1:]) - np.array(tpoints[:-1])
-plt.plot(tpoints[:-1], dtpoints, label = "Timestep") # drop the last point in tpoints
-plt.xlabel("Time t (s)")
-plt.ylabel("Timestep h (s)")
-plt.title('Size of Timestep over Time')
-plt.legend()
-plt.grid()
-plt.tight_layout()
 
 plt.show()
