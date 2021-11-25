128524798c191e45ba69e16ffe9131aa761aae66
Gabriel Bailey <gabriel.ken.bailey@gmail.com>
2021-11-25 16:18:17 -0500

    Lab10 Q3 Done.

--- Lab10/Lab10_Q3b.py
+++ Lab10/Lab10_Q3b.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
-'''Created on 2021-11-23
+'''Created on 2021-11-25
 
 @author: Gabriel Bailey
 
-This code will integrate x^(-1/2)/(1+e^x) from 0 to 1 using monte carlo methods, specifically mean-value and importance sampling. It will produce 2 histograms of the result, one for each method.
+This code will integrate e^(-2|x-5|) from 0 to 10 using monte carlo methods, specifically mean-value and importance sampling. It will produce 2 histograms of the result, one for each method.
 '''
 import matplotlib.pyplot as plt
 import numpy as np
 
 # The function we are integrating over
 def f(x):
-    return(x**(-1/2)/(1+np.exp(x)))
+    return(np.exp(-2*np.abs(x-5)))
 
 # f/w
 def f_over_w(x):
-    return(1/(1+np.exp(x)))
+    return((2*np.pi)**(1/2)*np.exp(np.abs(x-5)**2 /2 - 2*np.abs(x-5)))
 
 # Calculate the integral of f from a to b using mean value monte carlo with N iterations
 def integrate_mv(f,a,b,N):
@@ -29,11 +29,12 @@ def integrate_is(f,a,b,N):
     f_sum = 0
     for i in range(N):
         # Generate x based on the probability distrobution p
-        x = (np.random.random())**2
+        x = np.random.normal(5,1)
         
         f_sum += f(x)
-    # The 2 is the integral of w from a to b
-    return 2 * 1/N * f_sum
+    # The 1 is the integral of w from a to b (actually should be 0.9999...something but my calculator accuracy was too small)
+    # Ok ty wolfram alpha for 0.9999994267
+    return 0.9999994267 * 1/N * f_sum
 
 # Options
 compute_mv = True
@@ -46,7 +47,7 @@ repeats = 100
 f_mv = f
 f_is = f_over_w
 a = 0
-b = 1
+b = 10  
 
 # Arrays to put data into
 I_mv = np.empty(repeats)
@@ -57,18 +58,18 @@ if compute_mv:
     for i in range(repeats):
         I_mv[i] = integrate_mv(f_mv,a,b,N)
 
-    np.savez('Q3_mv_a', I_mv=I_mv)
+    np.savez('Q3_mv_b', I_mv=I_mv)
 else:
-    npzfile = np.load('Q3_mv_a.npz')
+    npzfile = np.load('Q3_mv_b.npz')
     I_mv = npzfile['I_mv']
 
 if compute_is:
     for i in range(repeats):
         I_is[i] = integrate_is(f_is,a,b,N)
 
-    np.savez('Q3_is_a', I_is=I_is)
+    np.savez('Q3_is_b', I_is=I_is)
 else:
-    npzfile = np.load('Q3_is_a.npz')
+    npzfile = np.load('Q3_is_b.npz')
     I_is = npzfile['I_is']
 
 # Plot the resulting mu histogram
@@ -76,12 +77,12 @@ fig, axmv = plt.subplots()
 axmv.set_title('Mean Value Integral Histogram') 
 axmv.set_xlabel('Calculated Value')
 axmv.set_ylabel('Count') 
-axmv.hist(I_mv, 10, range=[0.8, 0.88])
+axmv.hist(I_mv, 10, range=[0.95, 1.05])
 
 fig, axis = plt.subplots()
 axis.set_title('Importance Sampling Integral Histogram') 
 axis.set_xlabel('Calculated Value')
 axis.set_ylabel('Count') 
-axis.hist(I_is, 10, range=[0.8, 0.88])
+axis.hist(I_is, 10, range=[0.95, 1.05])
 
 plt.show()
